19cd1598610e0de42e9a1e0fcad6b707518d307e
俞寒轩 <yuhx402@gmail.com>
2014-10-29 11:06:38 +0800

    update mysql

--- sql/mySQl.sql
+++ sql/mySQl.sql
@@ -6,7 +6,20 @@ SET character_set_server='utf8';
 
 create table video (
     id int unsigned auto_increment primary key,
+    int unsigned type not null,
     title char(20) not null,
     path char(200) not null,
-    introduction char(200)
+    introduction char(200),
+    INDEX(type)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table admin (
+	int num primary key
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table comment(
+	id int unsigned primary key,
+	int unsigned seq not null,
+	content char(200) not null,
+	INDEX(seq)
+)
