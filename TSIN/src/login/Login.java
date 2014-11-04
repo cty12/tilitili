04d832f7f7f26de25f3c7c7aed48b52d2ff4f583
陈天昱 <chentianyu@outlook.com>
2014-11-04 18:12:53 +0800

    fix url encoding

--- TSIN/src/login/Login.java
+++ TSIN/src/login/Login.java
@@ -16,7 +16,7 @@ public class Login {
 		String result = "";
 		try {
 			URL loginUrl = new URL("http://student.tsinghua.edu.cn/practiceLogin.do");
-			String param = "userName=" + studentid + "&password=" + pwd;
+			String param = "userName=" + java.net.URLEncoder.encode(studentid) + "&password=" + java.net.URLEncoder.encode(pwd);
 			// 打开和URL之间的连接
 			URLConnection conn = loginUrl.openConnection();
 			// 设置通用的请求属性
